5b2267e2cf0f73d8cedb78581949aa279d534632
Pedro Lucas <izielucas2016@gmail.com>
2024-07-10 09:57:06 -0300

    added connection with mysql

--- 2_livraria/server.js
+++ 2_livraria/server.js
@@ -1,10 +1,27 @@
 import "dotenv/config";
-import express from 'express';
+import express, { json } from 'express';
 import mysql2 from "mysql2";
 import {v4 as uuidv4} from 'uuid';
 
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(json());
+
+const conn = mysql2.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Sen@iDev77!.',
+    database:'livraria',
+    port:3306
+});
+
+conn.connect((err) => {
+    if(err){
+        console.error(err.stack);
+    }
+    console.log("Mysql Conectado")
+});
 
 app.get("/", (req, res) =>{
     res.send("Olá Mundo");
