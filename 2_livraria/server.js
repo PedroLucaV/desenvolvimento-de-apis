f4207e345c1ddb6ef6cfc04656e6acc8bc004f7a
Pedro Lucas <izielucas2016@gmail.com>
2024-07-10 09:47:35 -0300

    server.js configured

---
+++ 2_livraria/server.js
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+import "dotenv/config";
+import express from 'express';
+import mysql2 from "mysql2";
+import {v4 as uuidv4} from 'uuid';
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.get("/", (req, res) =>{
+    res.send("Olá Mundo");
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor aberto na porta ${PORT}`)
+})
