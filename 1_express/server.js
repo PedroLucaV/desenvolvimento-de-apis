ac0e64cef6af6b5425bf1a9219fe5af546262d67
Pedro Lucas <izielucas2016@gmail.com>
2024-07-10 08:24:06 -0300

    created about params (query, route and body)

--- 1_express/server.js
+++ 1_express/server.js
@@ -1,9 +1,17 @@
-import express from "express";
+import express, { json } from "express";
 const PORT = 3333;
 
 const app = express();
 
-app.get("/users", (req, res) => {
+//aceitar dados JSON
+app.use(express.json());
+
+//Query params
+app.get("/users", (req, res) => { //trabalhando com as querys params
+    // const query = req.query;
+    // console.log(query)
+    const {nome, idade} = req.query; //desestruturando o objeto do query
+    console.log(nome, idade);
     res.status(200).json([
         "pessoa 1",
         "pessoa 2",
@@ -11,7 +19,10 @@ app.get("/users", (req, res) => {
     ])
 });
 
+//Body params
 app.post("/users", (req, res) => {
+    const {nome, idade} = req.body; //requisição do corpo desestruturada
+    console.log(nome, idade);
     res.status(201).json([
         "pessoa 1",
         "pessoa 2",
@@ -20,7 +31,13 @@ app.post("/users", (req, res) => {
     ])
 });
 
-app.put("/users", (req, res) => {
+//Route params
+app.put("/users/:id/:cpf", (req, res) => {
+    // const id = req.params.id; //podemos pegar dessa forma
+    // const cpf = req.params.cpf //e tambem podemos pegar multiplos parametros de rota
+    //quanto podemos desestruturar
+    const {id, cpf} = req.params;
+    console.log(id, cpf) 
     res.status(200).json([
         "pessoa 1",
         "pessoa 10",
