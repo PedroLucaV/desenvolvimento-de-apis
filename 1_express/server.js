ce984e6d36503645269ae47c4cf8f8160e2bb163
Pedro Lucas <izielucas2016@gmail.com>
2024-07-10 07:45:47 -0300

    server js com um codigo base

--- 1_express/server.js
+++ 1_express/server.js
@@ -4,23 +4,37 @@ const PORT = 3333;
 const app = express();
 
 app.get("/users", (req, res) => {
-    res.status(200).json({msg: "GET"})
+    res.status(200).json([
+        "pessoa 1",
+        "pessoa 2",
+        "pessoa 3"
+    ])
 });
 
 app.post("/users", (req, res) => {
-    res.status(200).json({msg: "POST"})
+    res.status(201).json([
+        "pessoa 1",
+        "pessoa 2",
+        "pessoa 3",
+        "pessoa 4"
+    ])
 });
 
 app.put("/users", (req, res) => {
-    res.status(200).json({msg: "PUT"})
+    res.status(200).json([
+        "pessoa 1",
+        "pessoa 10",
+        "pessoa 3",
+        "pessoa 4"
+    ])
 });
 
 app.delete("/users", (req, res) => {
-    res.status(200).json({msg: "DELETE"})
-});
-
-app.patch("/users", (req, res) => {
-    res.status(200).json({msg: "PATCH"})
+    res.status(204).json([
+        "pessoa 10",
+        "pessoa 3",
+        "pessoa 4"
+    ])
 });
 
 app.listen(PORT, () => {
