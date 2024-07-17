feb49c6d74dadf1d3f22e0055e2164a7669ea2e2
Pedro Lucas <izielucas2016@gmail.com>
2024-07-17 08:07:41 -0300

    created put route

--- 2_livraria/server.js
+++ 2_livraria/server.js
@@ -40,7 +40,7 @@ app.get("/livros", (req, res) =>{
 })
 
 app.post('/livros', (req, res) => {
-    const {titulo, autor, ano_publicacao, genero, preco, disponibilidade} = req.body;
+    const {titulo, autor, ano_publicacao, genero, preco} = req.body;
     if(!titulo){
         return res.status(400).json({message: "O titulo não pode ser vazio"});
     }
@@ -79,7 +79,7 @@ app.post('/livros', (req, res) => {
         const id = uuidv4();
         const insertSQL = /*sql*/ `
         INSERT INTO livros(id, titulo, autor, ano_publicacao, genero, preco, disponibilidade)
-        VALUES("${id}", "${titulo}", "${autor}", "${ano_publicacao}", "${genero}", "${preco}", "${disponibilidade}")`;
+        VALUES("${id}", "${titulo}", "${autor}", "${ano_publicacao}", "${genero}", "${preco}")`;
         conn.query(insertSQL, (err) => {
             if(err){
                 res.status(500).json({message: "erro ao cadastrar o livro"})
@@ -116,6 +116,58 @@ app.delete("/livro/:id", (req, res) => {
                 return console.error(err);
             }
             res.status(204);
+            res.end()
+        })
+    });
+})
+
+app.put("/livro/:id",(req, res) => {
+    const {id} = req.params;
+    const {titulo, autor, ano_publicacao, genero, preco, disponibilidade} = req.body;
+
+    if(!titulo){
+        return res.status(400).json({message: "O titulo não pode ser vazio"});
+    }
+    if(!autor){
+        return res.status(400).json({message: "O autor não pode ser vazio"});
+    }
+    if(!ano_publicacao){
+        return res.status(400).json({message: "O ano de publicacao não pode ser vazio"});
+    }
+    if(!genero){
+        return res.status(400).json({message: "O genero não pode ser vazio"});
+    }
+    if(!preco){
+        return res.status(400).json({message: "O preco não pode ser vazio"});
+    }
+    if(disponibilidade == undefined){
+        return res.status(400).json({message: "A disponibilidade não pode ser vazio"});
+    }
+
+    const checkSql = /*sql*/ `
+    SELECT * FROM livros
+    WHERE id = "${id}"`;
+    
+    conn.query(checkSql, (err, data) => {
+        if(err){
+            res.status(500).json({message: "erro ao buscar os livros"})
+            return console.error(err);
+        }
+        if(data.length == 0){
+            res.status(409).json({message: "Este livro não foi encontrado na base de dados!"});
+            return;
+        }
+        const updateSQL = /*sql*/ `
+        UPDATE livros
+        SET titulo = "${titulo}", autor = "${autor}", ano_publicacao = "${ano_publicacao}", genero = "${genero}", preco = "${preco}", disponibilidade = "${disponibilidade}"
+        WHERE id = "${id}"
+        `
+        conn.query(updateSQL, (err) => {
+            if(err){
+                res.status(500).json({message: "erro ao deletetar o livro"})
+                return console.error(err);
+            }
+            res.status(200).json({message: `Livro ${titulo} atualizado com sucesso!`});
             res.end()
         })
     });
