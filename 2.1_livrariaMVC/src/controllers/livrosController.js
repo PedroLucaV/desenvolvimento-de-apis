c18fff13ef3fc9e10b6b21c468ccb2705ac83396
Pedro Lucas <izielucas2016@gmail.com>
2024-07-20 08:41:20 -0300

    created other routes

--- 2.1_livrariaMVC/src/controllers/livrosController.js
+++ 2.1_livrariaMVC/src/controllers/livrosController.js
@@ -61,3 +61,122 @@ export const criarLivro = (req, res) => {
         });
     })
 }
+
+export const editarLivro = (req, res) => {
+    const {id} = req.params;
+    const {titulo, autor, ano_publicacao, genero, preco, disponibilidade} = req.body;
+
+    if(!titulo){
+        return res.status(400).json({message: "O titulo não pode ser vazio"});
+    }
+    if(!autor){
+        return res.status(400).json({message: "O autor não pode ser vazio"});
+    }
+    if(!ano_publicacao){
+        return res.status(400).json({message: "O ano de publicacao não pode ser vazio"});
+    }
+    if(!genero){
+        return res.status(400).json({message: "O genero não pode ser vazio"});
+    }
+    if(!preco){
+        return res.status(400).json({message: "O preco não pode ser vazio"});
+    }
+    if(disponibilidade == undefined){
+        return res.status(400).json({message: "A disponibilidade não pode ser vazio"});
+    }
+
+    const checkSql = /*sql*/ `
+    SELECT * FROM livros
+    WHERE id = "${id}"`;
+    
+    conn.query(checkSql, (err, data) => {
+        if(err){
+            res.status(500).json({message: "erro ao buscar os livros"})
+            return console.error(err);
+        }
+        if(data.length == 0){
+            res.status(409).json({message: "Este livro não foi encontrado na base de dados!"});
+            return;
+        }
+        const updateSQL = /*sql*/ `
+        UPDATE livros
+        SET titulo = "${titulo}", autor = "${autor}", ano_publicacao = "${ano_publicacao}", genero = "${genero}", preco = "${preco}", disponibilidade = "${disponibilidade}"
+        WHERE id = "${id}"
+        `
+        conn.query(updateSQL, (err) => {
+            if(err){
+                res.status(500).json({message: "erro ao deletetar o livro"})
+                return console.error(err);
+            }
+            res.status(200).json({message: `Livro ${titulo} atualizado com sucesso!`});
+            res.end()
+        })
+    });
+}
+
+export const deletarLivro = (req, res) => {
+    const {id} = req.params;
+    
+    const deleteSQL = /*sql*/ `
+    DELETE FROM livros
+    WHERE id = "${id}"
+    `
+    conn.query(deleteSQL, (err, info) => {
+        if(err){
+            res.status(500).json({message: "erro ao deletetar o livro"})
+            return console.error(err);
+        }
+        if(info.affectedRows == 0){
+            res.status(404).json({message: "Livro não encontrado na base de dados"})
+            return;
+        }
+        res.status(204);
+        res.end()
+    })
+}
+
+export const pegarLivroPorId = (req, res) => {
+    const {id} = req.params;
+    
+    const checkSql = /*sql*/ `
+    SELECT * FROM livros
+    WHERE id = "${id}"`;
+    
+    conn.query(checkSql, (err, data) => {
+        if(err){
+            res.status(500).json({message: "erro ao buscar os livros"})
+            return console.error(err);
+        }
+        const buscaLivro = data.some(livro => livro.id == id);
+        if(buscaLivro == false){
+            res.status(409).json({message: "Este livro não foi encontrado na base de dados!"});
+            return;
+        }
+        const livro = data;
+        res.status(200).json(livro);
+    });
+}
+
+export const pegarLivroPorNome = (req, res) => {
+    const nome = req.query.nome.replace('+', ' ');
+    // const nome = req.query.nome; //auto replace " " to %20 in web
+    
+    const checkSql = /*sql*/ `
+    SELECT * FROM livros
+    WHERE titulo like "${nome}%"`;
+    
+    conn.query(checkSql, (err, data) => {
+        console.log(checkSql)
+        if(err){
+            res.status(500).json({message: "erro ao buscar os livros"})
+            return console.error(err);
+        }
+        if(data.length == 0){
+            res.status(409).json({message: "Este livro não foi encontrado na base de dados!"});
+            return;
+        }
+
+        const livros = data;
+        res.status(200).json(livros);
+    });
+}
--- 2.1_livrariaMVC/src/routes/livroRoutes.js
+++ 2.1_livrariaMVC/src/routes/livroRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { pegarLivros, criarLivro } from "../controllers/livrosController.js";
+import { pegarLivros, criarLivro, editarLivro, deletarLivro, pegarLivroPorId, pegarLivroPorNome } from "../controllers/livrosController.js";
 
 const router = Router();
 
-router.get('/', pegarLivros)
-router.post('/', criarLivro)
+router.get('/', pegarLivros);
+router.post('/', criarLivro);
+router.put('/:id', editarLivro);
+router.delete('/:id', deletarLivro);
+router.get('/:id', pegarLivroPorId);
+router.get('/:nome', pegarLivroPorNome);
 
 export default router;
