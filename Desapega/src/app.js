b26f23b376076eec9b8934602faf9741472481c3
Wendel <wendel824829has@gmail.com>
2024-07-29 08:27:59 -0300

    created route 404 and validate conection with database

--- Desapega/src/app.js
+++ Desapega/src/app.js
@@ -1,9 +1,17 @@
-import Express from "express";
-import conn from "./configs/dbconfig.js";
+import Express, { json } from "express";
 import "dotenv/config";
+import conn from "./configs/dbconfig.js";
 
-const PORT = 8080;
+
+const PORT = process.env.PORT;
 const app = Express();
+
+app.use(json());
+
+app.use('*', (req, res) => {
+    res.status(404).json("Rota não encontrada!");
+    res.end();
+})
 
 app.listen(PORT, () => {
     console.log("Server Open on Port "+ PORT);
--- Desapega/src/configs/dbconfig.js
+++ Desapega/src/configs/dbconfig.js
@@ -2,12 +2,20 @@ import mysql from 'mysql2';
 
 const conn = mysql.createPool({
     connectionLimit: 10,
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE,
-    port:process.env.DB_PORT
+    host:process.env.MYSQL_HOST,
+    user:process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE,
+    port:process.env.MYSQL_PORT,
 });
+
+conn.query('SELECT 1 + 1 AS solution', (err, result, fields) => {
+    if(err){
+        return console.error(err);
+    }
+
+    console.log("The solution is: ", result[0].solution)
+})
 
 try {
     console.log("Mysql conectado")
